73373c96becf6569daeccca9f43235c974535c02
YongHun Byun <yonghunbyun@gmail.com>
2014-06-24 18:24:43 +0900

    config: Add production config

---
+++ app/config/production/app.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+
+    'url' => 'https://rivario.com',
+    'timezone' => 'Asia/Seoul',
+    'key' => 'yNO4eQr9vk80wpSHSuMDvheHNs1Io7ba'
+);
---
+++ app/config/production/session.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+
+    'cookie' => 'rivaio.com_session',
+);
