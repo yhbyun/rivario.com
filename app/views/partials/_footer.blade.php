089d439caab28159a4d504be49b8548475fcd705
YongHun Byun <yonghunbyun@gmail.com>
2014-06-27 01:32:53 +0900

    design: Fix the markup error

--- app/views/partials/_footer.blade.php
+++ app/views/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <div id="footer">
     <div class="container">
-        <p class="text-muted credit">Website built with <a href="http://laravel.com">Laravel</a> by <a target="_blank" href="http://about.me/yhbyun">YongHun Byun</a></a>
+        <p class="text-muted credit">Website built with <a href="http://laravel.com">Laravel</a> by <a target="_blank" href="http://about.me/yhbyun">YongHun Byun</a>
         <span class="pull-right">
         <a target="_blank" href="http://twitter.com/river" title="Follow updates"><i class="fa fa-twitter fa-lg"></i></a>
         |
