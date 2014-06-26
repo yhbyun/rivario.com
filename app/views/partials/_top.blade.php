a68b2e959e6ff3cd05f66c65801c2275c8d3ca36
YongHun Byun <yonghunbyun@gmail.com>
2014-06-27 01:32:27 +0900

    design: Change menu title

--- app/views/partials/_top.blade.php
+++ app/views/partials/_top.blade.php
@@ -17,7 +17,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-desktop"></i> Laravel <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="https://laravelrocks.com">Laravel Rocks</a></li>
+                        <li><a href="https://laravelrocks.com">laravelrocks.com</a></li>
                         <li><a href="{{ str_replace('/index', '', action('EloquentController@getIndex')) }}">Eloquent Test</a></li>
                     </ul>
                 </li>
