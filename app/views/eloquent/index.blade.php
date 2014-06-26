398c9f51f1ba38bd81f2f671c0916642dbf400e8
YongHun Byun <yonghunbyun@gmail.com>
2014-06-26 11:36:43 +0900

    Add disqus

--- app/views/eloquent/index.blade.php
+++ app/views/eloquent/index.blade.php
@@ -290,6 +290,24 @@ $results = $runner->getResult();
             </div>
         </div>
     </div>
+
+    <div class="row disqus">
+        <div class="col-xs-12">
+            <div id="disqus_thread"></div>
+            <script type="text/javascript">
+                var disqus_shortname = '{{ Config::get("config.disqus_shortname") }}';
+                var disqus_identifier = 'eloquent';
+
+                (function() {
+                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+        </div>
+    </div>
 </div>
 
 @stop
