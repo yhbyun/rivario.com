2ce384037b31c3200fd9acef5fe2ffe32f5c0d62
YongHun Byun <yonghunbyun@gmail.com>
2014-06-28 03:50:51 +0900

    Code cleanup

--- app/views/partials/_page-title.blade.php
+++ app/views/partials/_page-title.blade.php
@@ -1,17 +1,17 @@
 @if (isset($page_title))
-    @section('page_title')
-        <title>{{{ $page_title }}}</title>
+@section('page_title')
+<title>{{{ $page_title }}}</title>
 @stop
 @endif
 
 @if (isset($page_desc))
-    @section('page_desc')
-        <meta name="description" content="{{{ $page_desc }}}">
-    @stop
+@section('page_desc')
+<meta name="description" content="{{{ $page_desc }}}">
+@stop
 @endif
 
 @if (isset($page_keywords))
-    @section('page_keywords')
-        <meta name="keywords" content="{{{ $page_keywords }}}">
-    @stop
+@section('page_keywords')
+<meta name="keywords" content="{{{ $page_keywords }}}">
+@stop
 @endif
