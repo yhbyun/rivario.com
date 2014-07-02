8f70ce2b48348ab17cfa2fa3e03df157c6a7c8a0
YongHun Byun <yonghunbyun@gmail.com>
2014-07-02 16:45:04 +0900

    design: Split top menu

--- app/views/partials/_top.blade.php
+++ app/views/partials/_top.blade.php
@@ -12,8 +12,6 @@
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="/">Home</a></li>
-                <li><a href="http://ghost.rivario.com"><i class="fa fa-leaf"></i> Ghost Blog</a></li>
-                <li><a href="http://river.ecplaza.net"><i class="fa fa-leaf"></i> Blog</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-desktop"></i> Laravel <b class="caret"></b></a>
                     <ul class="dropdown-menu">
@@ -30,6 +28,10 @@
                         <li><a href="{{ str_replace('/index', '', action('CampaignsController@getIndex')) }}">CSS Inliner Tool</a></li>
                     </ul>
                 </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="http://ghost.rivario.com"><i class="fa fa-leaf"></i> Ghost Blog</a></li>
+                <li><a href="http://river.ecplaza.net"><i class="fa fa-leaf"></i> Blog</a></li>
                 <li><a href="http://about.me/yhbyun"><i class="fa fa-question-circle"></i> About</a></li>
             </ul>
         </div><!--/.nav-collapse -->
