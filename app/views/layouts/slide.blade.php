b39c64741ee91ab2381908ee9a19494f88c90f77
YongHun Byun <yonghunbyun@gmail.com>
2014-08-07 15:45:14 +0900

    Fix the title open graph meta tag error

--- app/views/layouts/slide.blade.php
+++ app/views/layouts/slide.blade.php
@@ -10,7 +10,7 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <meta property="og:title" content="gulp &middot; rivario.com" />
+    <meta property="og:title" content="@yield('title') &middot; rivario.com" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ URL::current() }}" />
     <meta property="og:image" content="@yield('image')" />
